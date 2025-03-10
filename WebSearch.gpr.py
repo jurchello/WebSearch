e6cd1d3548e18a3794e1485484c57fcead41f9f2
jurchello <devworkit@gmail.com>
2025-03-10 22:47:55 +0200

    Show visited icon for urls which were visited #3

--- WebSearch.gpr.py
+++ WebSearch.gpr.py
@@ -4,7 +4,7 @@ register(
     name=_("Web Search Gramplet"),
     description=_("Lists useful genealogy-related websites for research."),
     status=STABLE,
-    version="1.0.2",
+    version="1.0.3",
     fname="WebSearch.py",
     height=20,
     detached_width=400,
