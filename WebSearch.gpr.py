2833f27c81746d689b200b4298aa4048e5a52a3e
jurchello <devworkit@gmail.com>
2025-03-15 14:06:52 +0200

    Move classes to separate *.py files #16

--- WebSearch.gpr.py
+++ WebSearch.gpr.py
@@ -4,7 +4,7 @@ register(
     name=_("Web Search Gramplet"),
     description=_("Lists useful genealogy-related websites for research."),
     status=STABLE,
-    version="0.8.2",
+    version="0.8.3",
     fname="WebSearch.py",
     height=20,
     detached_width=400,
