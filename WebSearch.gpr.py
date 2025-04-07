848a6a8f76b244b6783f4567d1371e8728468798
jurchello <devworkit@gmail.com>
2025-04-07 18:55:05 +0300

    Change the "cross" icon background color to differ from other brue icons: "attribute", "note", and "internet" #102

--- WebSearch.gpr.py
+++ WebSearch.gpr.py
@@ -37,7 +37,7 @@ register(
         "Person, Place, Family, or Source record"
     ),
     status=STABLE,
-    version="0.69.46",
+    version="0.70.46",
     fname="WebSearch.py",
     height=20,
     detached_width=400,
