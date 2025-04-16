5f0e00785227e96d46c07b6064d496ffc9222fbf
jurchello <devworkit@gmail.com>
2025-04-16 22:57:53 +0300

    Auto-commit before publishing archives

--- WebSearch.gpr.py
+++ WebSearch.gpr.py
@@ -37,7 +37,7 @@ register(
         "Person, Place, Family, or Source record"
     ),
     status=STABLE,
-    version="0.80.58",
+    version="0.80.59",
     fname="WebSearch.py",
     height=20,
     detached_width=400,
