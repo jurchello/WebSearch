58b5b5bbb6e175b4aa5064044cb64a1089c793b0
jurchello <devworkit@gmail.com>
2025-04-23 14:48:40 +0300

    ValueError: not enough values to unpack (expected 2, got 0)

--- WebSearch.gpr.py
+++ WebSearch.gpr.py
@@ -37,7 +37,7 @@ register(
         "Person, Place, Family, or Source record"
     ),
     status=STABLE,
-    version="0.81.62",
+    version="0.81.63",
     fname="WebSearch.py",
     height=20,
     detached_width=400,
