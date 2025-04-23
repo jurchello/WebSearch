4fcbf9ba078962ae3910f6d2632fac36027576d2
jurchello <devworkit@gmail.com>
2025-04-23 11:24:03 +0300

    A new UA link added

--- WebSearch.gpr.py
+++ WebSearch.gpr.py
@@ -37,7 +37,7 @@ register(
         "Person, Place, Family, or Source record"
     ),
     status=STABLE,
-    version="0.80.60",
+    version="0.81.60",
     fname="WebSearch.py",
     height=20,
     detached_width=400,
