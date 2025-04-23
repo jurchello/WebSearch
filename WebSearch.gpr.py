0698fc6e1675ab350277673cbcf2f4a3cace671f
jurchello <devworkit@gmail.com>
2025-04-23 16:43:17 +0300

    0.81.69

--- WebSearch.gpr.py
+++ WebSearch.gpr.py
@@ -37,7 +37,7 @@ register(
         "Person, Place, Family, or Source record"
     ),
     status=STABLE,
-    version="0.81.68",
+    version="0.81.69",
     fname="WebSearch.py",
     height=20,
     detached_width=400,
